94bfb5e00b22a5c67897628f0a44bf32cf77915b
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2021-09-27 18:15:08 -0300

    add styles

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,12 +2,20 @@ import { Component } from 'react'
 
 import Product from './Product'
 
+const styles = {
+  products: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+}
+
 class Products extends Component {
   render() {
     const { products, addCart } = this.props
 
     return (
-      <div>
+      <div style={styles.products}>
         {products.map((product) => (
           <Product key={product.name} product={product} addCart={addCart} />
         ))}
