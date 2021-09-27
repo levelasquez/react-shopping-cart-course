ff004022a0a3d34cddebb1398547e11d75bd2e3a
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2021-09-27 17:56:57 -0300

    add initial state and product images

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  state = {
+    products: [
+      { name: 'Tomatoe', prices: 1.5, img: '/products/tomatoe.jpg' },
+      { name: 'Chickpeas', prices: 2.5, img: '/products/chickpeas.jpg' },
+      { name: 'Lettuce', prices: 0.5, img: '/products/lettuce.jpg' },
+    ],
+  }
+
   render() {
     return <p>Hola mundo</p>
   }
